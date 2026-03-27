f98d76737586995c75e134c0a2e06949be7f5a54
unknown <islamtaiyeba38@gmail.com>
2026-03-27 16:54:58 +0600

    test deploy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 Promise.all([connectDB(), redisService.connect()])
   .then(() => {
+    console.log("🔥 NEW CODE DEPLOYED");
     app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
   })
   .catch((err) => console.error("❌ Service connection failed:", err));
